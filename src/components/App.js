ce4d55df3dc5efddf86c96236cd52862f682dae6
lal15aditi <lal15aditi@gmail.com>
2023-12-19 14:18:59 +0530

    m

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,29 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [data , setData] = useState([]);
+  const [loading , setLoading] = useState(false);
+
+  function fetchData(){
+    setLoading(true);
+    fetch('https://dummyjson.com/products')
+      .then(response => response.json())
+      .then(response => {
+        setData(response);
+        setLoading(false);
+      })
+      .catch(error => console.log(error));
+  }
+  
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Data Fetch From Api</h1>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   )
 }
